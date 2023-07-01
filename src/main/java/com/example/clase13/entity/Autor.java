831146ecb5cc0c5dde82fd5bd0e13e9dd2ecb5b9
breyna2022 <C23898@utp.edu.pe>
2023-06-30 21:00:25 -0500

    v2.1

--- src/main/java/com/example/clase13/entity/Autor.java
+++ src/main/java/com/example/clase13/entity/Autor.java
@@ -1,6 +1,7 @@
 
 package com.example.clase13.entity;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,23 +9,27 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
- * @author Administrador
+ * @author admin
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "facultad")
-public class Facultad implements Serializable{
+@Table(name = "autor")
+public class Autor implements Serializable{
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="idfacultad")
+    @Column(name="idautor")
     private int id;
-    private String nombre;
+    private String nombres;
+    private String apellidos;
     private boolean estado;
-    
-    
+    private String foto;
 }
--- src/main/java/com/example/clase13/entity/Editorial.java
+++ src/main/java/com/example/clase13/entity/Editorial.java
@@ -8,23 +8,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
- * @author Administrador
+ * @author admin
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "facultad")
-public class Facultad implements Serializable{
+@Table(name = "editorial")
+public class Editorial implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="idfacultad")
+    @Column(name="ideditorial")
     private int id;
     private String nombre;
-    private boolean estado;
-    
-    
+    private String pais;
+    private boolean estado ;
 }
